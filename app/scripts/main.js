6cce69956db421a20851502d91146ea7769cef82
John Fahey <faheydesign@gmail.com>
2014-06-05 21:11:53 -0700

    Adding landing page layout and initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global CancerCode, $*/
+
+
+window.CancerCode = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    CancerCode.init();
+});
